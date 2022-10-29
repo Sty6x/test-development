0be824d389b2102da0b84b9fe593463db0f7c83a
diaz <jerylfranz460@gmail.com>
2022-10-29 14:23:45 +0400

    heh

--- src/index.js
+++ src/index.js
@@ -6,20 +6,46 @@ canvas.setAttribute('id', 'myChart');
 main.append(canvas)
 
 const ctx = document.getElementById('myChart').getContext('2d');
+const dataList = [{ temp: 38, date: 'wednesday' }, { temp: 45, date: 'tuesday' }, { temp: 23, date: 'thursday' },
+{ temp: 23, date: 'saturday' }, { temp: 23, date: 'friday' }, { temp: 38, date: 'wednesday' }, { temp: 45, date: 'tuesday' }, { temp: 23, date: 'thursday' },
+{ temp: 23, date: 'saturday' }, { temp: 23, date: 'friday' }]
+
+
+
+function outputData(dataList, what) {
+  const arb = []
+  if (what === 'temp') {
+    for (let i = 0; i < dataList.length; i++) {
+      arb.push(dataList[i].temp)
+    }
+    return arb
+  } else if (what === 'date') {
+
+    for (let i = 0; i < dataList.length; i++) {
+      arb.push(dataList[i].date)
+    }
+    return arb
+  }
+}
+const hourlyDate = outputData(dataList, 'date');
+const hourlyTemps = outputData(dataList, 'temp');
 const myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'some'],
+    labels: hourlyDate,
     datasets: [{
-      label: '# of Votes',
-      data: [42, 19, 3, 5, 2, 3], data: [{ x: 10, y: 20 }, { x: 15, y: null }, { x: 20, y: 10 }],
+      label: 'Hourly Temperature',
+      data: hourlyTemps,
+      borderRadius: 10,
+      borderWidth: 9,
+      // barThickness: 10,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(255, 99, 132, )',
+        'rgba(54, 162, 235, )',
+        'rgba(255, 206, 86, )',
+        'rgba(75, 192, 192, )',
+        'rgba(153, 102, 255,)',
+        'rgba(255, 159, 64, )'
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
@@ -29,10 +55,13 @@ const myChart = new Chart(ctx, {
         'rgba(153, 102, 255, 1)',
         'rgba(255, 159, 64, 1)'
       ],
-      borderWidth: 1
     }]
   },
   options: {
+
+    layout: {
+      padding: 200
+    },
     scales: {
       y: {
         beginAtZero: true

c155835e9fb2e109cf86ad00fe865b7a3dc14c5d
zaid <jerylfranz460@gmail.com>
2022-10-29 13:15:01 +0400

    yeet

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 import './style.css'
-
-
-const image = document.querySelector('img')
+import Chart from 'chart.js/auto';
 const main = document.querySelector('main')
-const btn = document.createElement('button')
-const search = document.createElement('input')
-search.setAttribute('type', 'text')
-btn.textContent = 'Get Image'
-main.append(btn, search)
+const canvas = document.createElement('canvas')
+canvas.setAttribute('id', 'myChart');
+main.append(canvas)
 
-
-async function getImg() {
-  try {
-    const grabImage = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=${search.value}`, { mode: 'cors' })
-    const extractBody = await grabImage.json()
-    image.src = extractBody.data.images.original.url
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'some'],
+    datasets: [{
+      label: '# of Votes',
+      data: [42, 19, 3, 5, 2, 3], data: [{ x: 10, y: 20 }, { x: 15, y: null }, { x: 20, y: 10 }],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
   }
-  catch (e) {
-    console.log(e)
-    const grabErrImage = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=nothing`, { mode: 'cors' })
-    const extractErrBody = await grabErrImage.json()
-    image.src = extractErrBody.data.images.original.url
-  }
-}
-
-btn.addEventListener('click', getImg)
+});
